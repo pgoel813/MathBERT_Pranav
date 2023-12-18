31d605dc8acf73e484e70abf17d8028efd9102a1
pgoel813 <147204476+pgoel813@users.noreply.github.com>
2023-12-18 13:56:25 -0600

    Updated feedback and answers

--- app.py
+++ app.py
@@ -88,6 +88,7 @@ embeddings, questions_db = load_embeddings()
 
 def get_similar_question(query, num_questions, model, question_embeddings, main_questions):
     answers_dict = []
+    questions_dict = []
     #embed the query
     query_embedding = model.encode(query, convert_to_tensor=False)
     query_embedding = query_embedding.astype(np.float32)
@@ -100,23 +101,36 @@ def get_similar_question(query, num_questions, model, question_embeddings, main_
     indexes = top_scores[1][0]
     #get related question
     for question_index in indexes:
+        questions_dict.append(main_questions[question_index])
         answers_dict.append(main_questions[question_index])
 
-    return answers_dict
+    return questions_dict, answers_dict
 
 
-def answers_holders(*args):
+def answers_holders(*args,answers):
 
-    for index, questions in enumerate(args):
-        answers_div = f"""
+    for index, (question, answer) in enumerate(zip(args, answers)):
+        question_div = f"""
             <div class="answers">
                 <h5>Similar Question: {index+1}</h5>
-                <p><strong>{questions}</strong></p>
+                <p><strong>{question}</strong></p>
             </div>
         """
-
         #set the div
-        st.markdown(answers_div, unsafe_allow_html = True)
+        st.markdown(question_div, unsafe_allow_html = True)
+        time.sleep(2)
+        answer = answer.replace('.', '**').replace('####', '**')
+        steps = answer.split('**')
+        for step in steps:
+            if step.strip():
+                answer_div = f"""
+                <div class="step">
+                <p><strong>{step.strip()}</strong></p>
+                </div>
+                """
+                st.markdown(answer_div, unsafe_allow_html = True)
+                time.sleep(2)
+        time.sleep(2)
 
 
 def update_feedback(interact_date, user_question, user_feedback):
@@ -196,27 +210,46 @@ with tab1:
             feedback_state = False
             button_state = True
 
-            with st.sidebar:
+                       with st.sidebar:
                 st.title("Feedback")
                 st.write("Your feedback is much appreciated for improvemment of the web application.")
-                st.subheader("Are You Satisfied with the Questions You Got?")
+                st.subheader("How helpful was this AI Generated Response towards helping you solve this probelm?")
 
                 #create columns
-                col1, col2 = st.columns(2)
+                col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
 
                 with col1:
                     #set feedback buttons
-                    if st.button("Yes", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
-                        feedback_state = update_feedback(datetime.now(), question, "Yes")
+                    if st.button("0 (Actively Harmful)", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
+                        feedback_state = update_feedback(datetime.now(), question, "0")
 
                 with col2:
-                    if st.button("No", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
-                        feedback_state = update_feedback(datetime.now(), question, "No")
+                    if st.button("1 (Very Harmful)", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
+                        feedback_state = update_feedback(datetime.now(), question, "1")
 
+                with col3:
+                    if st.button("2 (Somewhat Harmful)", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
+                        feedback_state = update_feedback(datetime.now(), question, "2")
+                      
+                with col4:
+                    if st.button("3 (Unlikely to help,but unlikely to hurt)", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
+                        feedback_state = update_feedback(datetime.now(), question, "3")     
+                      
+                with col5:
+                    if st.button("4 (Somewhat Helpful)", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
+                        feedback_state = update_feedback(datetime.now(), question, "4")
+                      
+                with col6:
+                    if st.button("5 (Very Helpful)", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
+                        feedback_state = update_feedback(datetime.now(), question, "5")
+                      
+                with col7:
+                    if st.button("6 (Extremely Helpful)", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
+                        feedback_state = update_feedback(datetime.now(), question, "6")
+ 
                 #say thank you if the feedback is given
                 if feedback_state:
                     st.header("Thank You For Your Feedback!")
-
 with tab2:
     st.header("Feedback Analysis")
     st.subheader("Feedback Data")
